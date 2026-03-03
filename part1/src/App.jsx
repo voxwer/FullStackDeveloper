a9748244d926c1e41a3821d994116d00d05fbac2
voxwer <tonnyantonii0@gmail.com>
2026-03-03 17:02:52 -0600

    Exercises 1.1 to 1.2 completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,32 @@
-const Hello = (props) => {
+const Hello = (props) =>{
   console.log(props)
   return (
     <div>
-      <p>Hello {props.name}, you are  {props.age} years old</p>
+      <p>Hello {props.name}</p>
     </div>
   )
 }
-const footer = () => {
+
+const Footer = () => {
   return (
     <div>
       greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
     </div>
   )
 }
+
 const App = () => {
-  const friends = [ 'Peter', 'Maya']
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
 
   return (
     <div>
-      <p>{friends}</p>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
     </div>
   )
 }
+
 export default App
